b986a134912b5625f85aa138fe7665587d85857b
PlacidPlaty <yitongng4.0@gmail.com>
2024-12-07 01:26:10 +0800

    created game window

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,13 +1,20 @@
 package main;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+/*
+ * Creates Game screen
+ */
+
+// subclass of JPanel
+public class GamePanel extends JPanel implements Runnable{
 
     // SCREEN SETTINGS
     final int ORIGINALTILESIZE = 16; // 16x16 tile size
     // need to scale 16X16 tile size or they will look small on modern screens
-    final int SCALE = 3; 
+    final int SCALE = 3;
 
     final int TILESIZE = ORIGINALTILESIZE * SCALE; // 48x48 tile size
 
@@ -16,5 +23,35 @@ public class GamePanel extends JPanel{
     final int MAXSCREENROW = 12;
     final int SCREENWIDTH = TILESIZE * MAXSCREENCOL; // 768 pixels
     final int SCREENHEIGHT = TILESIZE * MAXSCREENROW; // 576 pixels
+
+    // Create threads for multiple things running in game
+    Thread gameThread;
+
+    public GamePanel() {
+        // new Dimension(width, height)
+        this.setPreferredSize(new Dimension(SCREENWIDTH, SCREENHEIGHT));
+        this.setBackground(Color.black);
+
+        // If set to true, all the drawing from this component will be done in an
+        // offscreen painting buffer
+        this.setDoubleBuffered(true);
+    }
+
+    // method to start new thread
+    public void startGameThread() {
+        
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        /*
+         * Game Loop for the core of the game
+         */
+        // TODO Auto-generated method stub
+
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -9,6 +9,12 @@ public class Main {
         window.setResizable(false);
         window.setTitle("Harry Potter and the Mug on Fire");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        // causes this window to be iszed to fit the preferred size and layouts of it ssubcomponents (=GamePanel)
+        window.pack();
+
         window.setLocationRelativeTo(null); // set window to center of the screen
         window.setVisible(true);
     }   
